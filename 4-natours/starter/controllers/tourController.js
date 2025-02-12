01be04acdd5f6812319dede141f08aa59388d259
Kamal Yadav <kamal.yadav@Kamals-MacBook-Air.local>
2025-02-12 21:13:29 +0530

    async errors

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('./errorController.js'); //will work on taking the latest pull
 
 //ROUTE HANDLERS
 exports.aliasTopTours = async (req, res, next) => {
@@ -28,10 +29,14 @@ exports.getAllTours = catchAsync(async function (req, res, next) {
     });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
     const tour = await Tour.findById(req.params.id);
     //findById is similar to writing Tour.findOne({"_id": req.params.id}), basically a shorthand
     //But since mongoose gives us a helper function to directly use the findById method in case of finding by id
+
+    if (!tour) {
+        return next(new AppError('Cannot find Tour with that ID', 404));
+    }
 
     res.status(200).json({
         status: 'Success',
@@ -41,7 +46,7 @@ exports.getTour = catchAsync(async (req, res) => {
     });
 });
 
-exports.createTour = catchAsync(async (req, res) => {
+exports.createTour = catchAsync(async (req, res, next) => {
     const newTour = await Tour.create(req.body);
 
     res.status(201).json({
@@ -50,7 +55,7 @@ exports.createTour = catchAsync(async (req, res) => {
     });
 });
 
-exports.updateTour = catchAsync(async (req, res) => {
+exports.updateTour = catchAsync(async (req, res, next) => {
     //findByIdAndUpdate takes id, and second param is what you want to update, then we are passing one more option
     //new: true, which returns the modified document rather than the original
     //runValidators: to check if the req.body schema meets our originla schema
@@ -67,8 +72,12 @@ exports.updateTour = catchAsync(async (req, res) => {
     });
 });
 
-exports.deleteTour = catchAsync(async (req, res) => {
-    await Tour.findByIdAndDelete(req.params.id);
+exports.deleteTour = catchAsync(async (req, res, next) => {
+    const tour = await Tour.findByIdAndDelete(req.params.id);
+
+    if (!tour) {
+        return next(new AppError('Cannot find Tour with that ID', 404));
+    }
 
     res.status(204).json({
         status: 'success',
@@ -76,7 +85,7 @@ exports.deleteTour = catchAsync(async (req, res) => {
     });
 });
 
-exports.getTourStats = async (req, res) => {
+exports.getTourStats = async (req, res, next) => {
     const stats = await Tour.aggregate([
         { $match: { ratingsAverage: { $gte: 4.5 } } },
         {
@@ -102,7 +111,7 @@ exports.getTourStats = async (req, res) => {
 //A real world business problem where we are asked to show the busiest month like when the tour starts and all, so we can get a monthly plan
 //We want to check how many tours start in a particular month
 //We can then group the tours by those months
-exports.getMonthlyPlan = catchAsync(async (req, res) => {
+exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
     const year = +req.params.year;
     const plan = await Tour.aggregate([
         { $unwind: '$startDates' }, //unwind the array into different documents according to the array values
